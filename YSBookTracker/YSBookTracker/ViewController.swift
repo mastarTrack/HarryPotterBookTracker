ec6b1fa548a1d6e2d23472bcb641b7610b293839
yeseul jang <yyssjj9999@gmail.com>
2026-01-22 21:31:44 +0900

    🌟feat: 프로젝트 생성

---
+++ YSBookTracker/YSBookTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YSBookTracker
+//
+//  Created by Yeseul Jang on 1/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
