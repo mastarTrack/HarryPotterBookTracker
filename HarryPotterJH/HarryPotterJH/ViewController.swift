e0f03f496e1a931b4c7103c0f8234e4526ecd68c
coduhee <vdyjb@ewha.ac.kr>
2026-01-30 11:59:16 +0900

    chore: 리팩토링

--- HarryPotterJH/HarryPotterJH/ViewController.swift
+++ HarryPotterJH/HarryPotterJH/ViewController.swift
@@ -16,6 +16,7 @@ final class ViewController: UIViewController {
     var books: [Book] = []
     var index = 0
     private let dataService = DataService() // 데이터 담당자 생성
+    var isExpanded = false
     
     
     // MARK: -- UI Components
@@ -55,7 +56,7 @@ final class ViewController: UIViewController {
     // 책 메인 정보 스택뷰 (이미지 + 텍스트)
     let bookInfoStackView = UIStackView().then {
         $0.axis = .horizontal
-        $0.spacing = 5
+        $0.spacing = 15
         $0.alignment = .top
     }
     
@@ -147,21 +148,17 @@ final class ViewController: UIViewController {
         $0.font = .systemFont(ofSize: 18, weight: .bold)
     }
     
-    var isExpanded = false
-    
-    private let expandedKey = "isSummaryExpanded"
-    
     
     // MARK: - viewDidLoad()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        seriesButton1.backgroundColor = .systemGray5
+        seriesButton1.setTitleColor(.systemBlue, for: .normal)
         loadBooks()
-        seriesButton1.backgroundColor = .lightGray
         setupSubView()
         setupConstraints()
-        
     }
     
     
@@ -177,12 +174,14 @@ final class ViewController: UIViewController {
             case .failure(let error):
                 print("error: \(error)")
                 
+                // 에러 알림창(UI)는 메인스레드에 맞춰 실행
                 DispatchQueue.main.async {
                     self.showErrorAlert(message: error.localizedDescription)
                 }
             }
         }
     }
+    
     
     // MARK: -- add_Subview
     
@@ -197,7 +196,7 @@ final class ViewController: UIViewController {
         
         scrollView.addSubview(contentView)
         
-        [bookInfoStackView, dedicationStackView, summaryStackView, chapterStackView, summaryButton].forEach {
+        [bookInfoStackView, dedicationStackView, summaryStackView, summaryButton, chapterStackView].forEach {
             contentView.addSubview($0)
         }
         
@@ -261,10 +260,6 @@ final class ViewController: UIViewController {
             $0.height.equalTo(bookImageView.snp.width).multipliedBy(1.5)
         }
         
-        bookInfoTextStackView.snp.makeConstraints {
-            $0.leading.equalTo(bookImageView.snp.trailing).offset(10)
-        }
-        
         dedicationStackView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.top.equalTo(bookInfoStackView.snp.bottom).offset(24)
@@ -290,10 +285,13 @@ final class ViewController: UIViewController {
     
     // MARK: -- function
     
-    // 에러창 띄우는 함수
+    // 에러창(팝업) 띄우는 함수
     private func showErrorAlert(message: String) {
+        // 알림창 생성
         let alert = UIAlertController(title: "에러 발생", message: message, preferredStyle: .alert)
+        // 확인 버튼 생성
         alert.addAction(UIAlertAction(title: "확인", style: .default))
+        // 화면에 띄우기
         self.present(alert, animated: true)
     }
     
@@ -309,23 +307,56 @@ final class ViewController: UIViewController {
         return button
     }
     
-    // 책 정보뷰 텍스트 생성 함수
-    private func createInfoText(title: String, value: String, titleSize: CGFloat, valueSize: CGFloat, valueColor: UIColor) -> NSAttributedString {
-        let attrString = NSMutableAttributedString(
-            string: title,
-            attributes: [.font: UIFont.systemFont(ofSize: titleSize, weight: .bold), .foregroundColor: UIColor.black])
+    // date 포멧 변경 함수
+    private func formatDate(_ dateString: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
         
-        // 타이틀과 내용 사이에 간격8 추가
-        attrString.append(NSAttributedString(
-            string: "  \(value)",
-            attributes: [.font: UIFont.systemFont(ofSize: valueSize), .foregroundColor: valueColor]
-        ))
-        return attrString
+        if let date = formatter.date(from: dateString) { // 문자열을 date형식으로 변환
+            formatter.dateStyle = .long // 출력용 날짜 스타일
+            return formatter.string(from: date) // date를 문자열로 다시 변환
+        }
+        return dateString // 실패하면 원본 반환
+    }
+    
+    // 책 정보뷰 텍스트 생성 함수 (한 줄에 스타일 다른 글자 두 덩이 붙이기)
+    private func createInfoText(
+        title: String,
+        value: String,
+        titleSize: CGFloat,
+        valueSize: CGFloat,
+        valueColor: UIColor
+    ) -> NSAttributedString {
+        
+        // 1. 타이틀 속성 지정
+        let inforesult = NSMutableAttributedString(
+            string: title,
+            attributes: [
+                .font: UIFont.systemFont(ofSize: titleSize, weight: .bold),
+                .foregroundColor: UIColor.black])
+        
+        
+        // 2. 간격 설정
+        let spacing = NSAttributedString (string: " ", attributes: [.kern: 8]
+        )
+        inforesult.append(spacing)
+        
+        // 3. 내용 속성 지정
+        inforesult.append(
+            NSAttributedString(
+                string: value,
+                attributes: [
+                    .font: UIFont.systemFont(ofSize: valueSize),
+                    .foregroundColor: valueColor
+                ]
+            )
+        )
+        return inforesult
     }
     
     // UI 갱신 함수
     func updateUI() {
-        guard books.indices.contains(index) else { return } // count가 books 안에 진짜 존재할 때만 실행
+        guard books.indices.contains(index) else { return } // index가 books 안에 진짜 존재할 때만 실행
         let book = books[index]
         
         // 저장된 상태 불러오기 (디폴트값: false)
@@ -347,6 +378,8 @@ final class ViewController: UIViewController {
         
         dedicationLabel.text = book.dedication
         
+        summaryLabel.text = book.summary
+        
         // 챕터 스택 뷰의 모든 서브뷰 초기화
         chapterStackView.subviews.forEach{ $0.removeFromSuperview() }
         
@@ -365,9 +398,6 @@ final class ViewController: UIViewController {
             chapterStackView.addArrangedSubview(label)
         }
         
-        // Summary 레이블에 값 대입
-        summaryLabel.text = book.summary
-        
         // 버튼의 초기 텍스트 수정
         if book.summary.count > 450 {
             // 줄 수 제한
@@ -378,26 +408,14 @@ final class ViewController: UIViewController {
             } else {
                 summaryButton.setTitle("더 보기", for: .normal)
                 // 450자에서 자르고 "..." 붙이기
-                let index = book.summary.index(book.summary.startIndex, offsetBy: 450)
-                summaryLabel.text = String(book.summary[..<index]) + "..."
+                let indexing = book.summary.index(book.summary.startIndex, offsetBy: 450)
+                summaryLabel.text = String(book.summary[..<indexing]) + "..."
             }
         } else {
-            summaryButton.isHidden = true // 450자가 넘으면 버튼 숨길필요없이 내용 전체 대입
+            summaryButton.isHidden = true // 450자가 넘으면 버튼 숨기고 내용 전체 대입
             summaryLabel.text = book.summary
         }
         
-    }
-    
-    // date 포멧 변경 함수
-    private func formatDate(_ dateString: String) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        
-        if let date = formatter.date(from: dateString) {
-            formatter.dateStyle = .long
-            return formatter.string(from: date)
-        }
-        return dateString
     }
     
     
@@ -407,12 +425,16 @@ final class ViewController: UIViewController {
     @objc
     private func seriesButtonTapped(_ sender: UIButton) {
         if let title = sender.currentTitle {
-            [seriesButton1, seriesButton2, seriesButton3, seriesButton4, seriesButton5, seriesButton6, seriesButton7].forEach {$0.backgroundColor = .systemBlue}
+            [seriesButton1, seriesButton2, seriesButton3, seriesButton4, seriesButton5, seriesButton6, seriesButton7
+            ].forEach {
+                $0.backgroundColor = .systemBlue
+                $0.setTitleColor(.white, for: .normal)}
             
             // tap한 버튼의 title - 1값이 index
             index = (Int(title) ?? 0 ) - 1
-            // 선택된 버튼의 배경색 변경
-            sender.backgroundColor = .systemGray4
+            // 선택된 버튼의 배경색, 글자색 변경
+            sender.backgroundColor = .systemGray5
+            sender.setTitleColor(.systemBlue, for: .normal)
             updateUI()
         }
     }
@@ -438,9 +460,12 @@ final class ViewController: UIViewController {
             summaryButton.setTitle("더 보기", for: .normal)
         }
     }
-    
-    
 }
+
+
+
+
+
 
 
 @available(iOS 17.0, *)
